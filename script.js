a3283c5f85591f9e649b42d7dc2f73e5978a3890
Jagannathan CS <jagannathan606@gmail.com>
2023-04-16 09:57:18 +0530

    Optimized function X to improve performance by reducing its time complexity

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+///////////////////the nav bar sidemenu for mobile screen 
+var sidemeu = document.getElementById("sidemenu");
+function openmenu(){
+    sidemeu.style.right = "0";
+}
+function closemenu(){
+    sidemeu.style.right = "-200px";
+}
+
+/////////////////////skills and experience slider funciton
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
@@ -7,33 +17,21 @@ function opentab(tabname){
          tablinks[i].classList.remove('active-link');
       }
    }
-
    for (var j in tabcontents) {
       if (tabcontents.hasOwnProperty(j)) {
          tabcontents[j].classList.remove('active-tab');
       }
    }
-
    event.currentTarget.classList.add("active-link");
    document.getElementById(tabname).classList.add("active-tab");
 }
 
 
-var sidemeu = document.getElementById("sidemenu");
 
-function openmenu(){
-    sidemeu.style.right = "0";
-
-}
-function closemenu(){
-    sidemeu.style.right = "-200px";
-}
-
-// google shit linking code and github script with source key
+/////////////////////google shit linking code and github script with source key
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxDxrjRr52CIv8mcBiOa-ytC5L8e9jFUq6MnHZcbcmpiXu9koilzVfxhuCHactIw9Nm/exec'
 const form = document.forms['submit-to-google-sheet']
 const msg= document.getElementById("msg");
-
 
 form.addEventListener('submit', e => {
   e.preventDefault()
@@ -51,3 +49,9 @@ form.addEventListener('submit', e => {
  });
  
 })
+
+///////////////////////alert about under processs
+function showAlert() {
+   alert("We're sorry, our site is currently under construction. Please check back soon for updates!");
+ }
+ 
